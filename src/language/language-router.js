674265b28f47bb1236801746443408d5c6036558
andrewbituin <fandrewbituin@gmail.com>
2019-04-17 16:57:03 -0700

    implementing db updates via a new linked list

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 const express = require("express");
 const LanguageService = require("./language-service");
 const { requireAuth } = require("../middleware/jwt-auth");
-const  {LinkedList, _Node}  = require('./LinkedList');
+const { LinkedList, _Node } = require("./LinkedList");
 // const doLinkedList = require('./language-service');
 
 const languageRouter = express.Router();
@@ -53,42 +53,17 @@ languageRouter.get("/", async (req, res, next) => {
 // GET request handler for rendering each learning page at 'api/language/head'
 languageRouter.get("/head", async (req, res, next) => {
   try {
-    const words = await LanguageService.getLanguageWords(
+    const [nextWord] = await LanguageService.getNextWord(
       req.app.get("db"),
       req.language.id
     );
-    const language = await LanguageService.getUsersLanguage(
-      req.app.get("db"),
-      req.user.id
-    );
-
-    const linkedList = new LinkedList();
-    words.map(word => linkedList.insertLast(word));
-
-    if (!language) {
-      return res.status(404).json({
-        error: `You don't have any languages`
-      });
-    }
-
-    let currNode = linkedList.head;
-
-    // console.log(linkedList);
-    // console.log(currNode);
-
-    // res.json({
-    //   nextWord: words[1].original,
-    //   totalScore: words[0].total_score,
-    //   wordCorrectCount: words[0].correct_count,
-    //   wordIncorrectCount: words[0].incorrect_count
-    // });
-
     res.json({
-      nextWord: currNode.value.original,
-      totalScore: language.total_score,
-      wordCorrectCount: currNode.value.correct_count,
-      wordIncorrectCount: currNode.value.incorrect_count
+      nextWord: nextWord.original,
+      totalScore: req.language.total_score,
+      wordCorrectCount: nextWord.correct_count,
+      wordIncorrectCount: nextWord.incorrect_count
     });
+    next();
   } catch (error) {
     next(error);
   }
@@ -96,33 +71,32 @@ languageRouter.get("/head", async (req, res, next) => {
 
 // POST request handler for user guessing
 languageRouter.post("/guess", bodyParser, async (req, res, next) => {
-
   const { guess } = req.body;
   const newGuess = guess;
 
   try {
     if (!req.body.guess) {
-      res.status(400).json({error: `Missing 'guess' in request body`});
-      return
+      res.status(400).json({ error: `Missing 'guess' in request body` });
+      return;
     }
-
-    // console.log(req.body)
-
     const words = await LanguageService.getLanguageWords(
       req.app.get("db"),
       req.language.id
     );
-    
+
     const language = await LanguageService.getUsersLanguage(
       req.app.get("db"),
       req.user.id
     );
+    //need a function that creates the linked list one at a time based on first the head, then each .next after
+    // const linkedList = new LinkedList();
+    // words.map(word => linkedList.insertLast(word));
 
-    const linkedList = new LinkedList();
-    words.map(word => linkedList.insertLast(word));
+    const list = LanguageService.createLinkedList(words, language.head)
+    // console.log(JSON.stringify(list, null, 2))
 
     let isCorrect;
-    let currNode = linkedList.head;
+    let currNode = list.head;
 
     let answerPrev = currNode.value.translation;
 
@@ -133,8 +107,7 @@ languageRouter.post("/guess", bodyParser, async (req, res, next) => {
       currNode.value.memory_value *= 2;
       // currNode.next = currNode.next ? currNode.next.value.id : null; // need help understanding this!!
       //linkedList.head = currNode.next;
-    } 
-    else {
+    } else {
       isCorrect = false;
       currNode.value.incorrect_count += 1;
       currNode.value.memory_value = 1;
@@ -149,168 +122,72 @@ languageRouter.post("/guess", bodyParser, async (req, res, next) => {
       wordCorrectCount: currNode.value.correct_count,
       wordIncorrectCount: currNode.value.incorrect_count
     };
-  
-    res.status(200).json(result)
+
+    res.status(200).json(result);
 
     // shift node to correct position by M spaces
 
-    let newNext = currNode.next.value.id;
-
-
-    // console.log(JSON.stringify(linkedList, null, 2));
-
-    console.log(currNode.value.memory_value);
-
-  
-    //linkedList.remove(currNode);
-
-    // console.log(linkedList);
-
-    //implementing move currNode according to memory value stemming from user correct/incorrect response
-
-    // function Node(data, next = null) {
-    //   this.data = data, this.next = next;
-    // }
-    // function Context(source, dest) {
-    //   this.source = source, this.dest = dest;
-    // }
-    // function moveNode(source, dest) {
-    //   if(!source) throw "Error"
-    //   return new Context(source.next, new Node(source.data, dest));
-    // }
-
-    // moveNode(currNode, currNode.value.memory_value + 1);
-    // linkedList.remove(currNode);
-
-      // move current node back in the list depending on how user responds
-
-    // console.log(newNext);
-    // if (currNode.value.memory_value === 1) {
-    //   if (currNode.next !== null) {
-    //     newNext = (currNode.next.value.id + 1);
-    //   }
-    //   if (currNode.next === null) {
-    //     newNext = (currNode.next.value.id + 1) - words.length;
-    //     // linkedList.find() // wanted to find 'next' node and update pointer
-    //   }
-    // }
-
-    // if (currNode.value.memory_value !== 1) {
-    //   if (currNode.next !== null) {
-    //     newNext = (currNode.next.value.id + currNode.value.memory_value);
-    //   }
-    //   if (currNode.next === null) {
-    //     newNext = (currNode.next.value.id + currNode.value.memory_value) - words.length;
-    //   }
-    //   if (newNext > words.length) {
-    //     newNext -= words.length;
-    //   }
-    // }
-    // console.log(newNext);
-
-    // post updated values to database
-    console.log(JSON.stringify(linkedList, null, 2))
-    // linkedList.insertAt(currNode, currNode.value.memory_value)
-
-    let curr = linkedList.head
-    let countDown = currNode.value.memory_value
-    while(countDown > 0 && curr.next !== null){
-      curr = curr.next
+    let curr = list.head;
+    let countDown = currNode.value.memory_value;
+    while (countDown > 0 && curr.next !== null) {
+      curr = curr.next;
       countDown--;
     }
-    const temp = new _Node(linkedList.head.value)
+    const temp = new _Node(list.head.value);
 
-    if(curr.next === null){
-      temp.next = curr.next
-      curr.next = temp
-      linkedList.head = linkedList.head.next
-      curr.value.next = temp.value.id
-      temp.value.next = null
+    if (curr.next === null) {
+      temp.next = curr.next;
+      curr.next = temp;
+      list.head = list.head.next;
+      curr.value.next = temp.value.id;
+      temp.value.next = null;
     } else {
-      temp.next = curr.next
-      curr.next = temp
-      linkedList.head = linkedList.head.next
-      curr.value.next = temp.value.id
-      temp.value.next = temp.next.value.id
+      temp.next = curr.next;
+      curr.next = temp;
+      list.head = list.head.next;
+      curr.value.next = temp.value.id;
+      temp.value.next = temp.next.value.id;
     }
 
-    //console.log(JSON.stringify(linkedList, null, 2))
+    //console.log(JSON.stringify(list, null, 2))
 
-    // convert linkedList into an array
-    let newArray = []
-    let newCurrNode = linkedList.head
-    while(newCurrNode.next !== null){
-      newArray.push(newCurrNode.value)
-      newCurrNode = newCurrNode.next
+    // convert list into an array
+    let newArray = [];
+    let newCurrNode = list.head;
+    while (newCurrNode.next !== null) {
+      newArray.push(newCurrNode.value);
+      newCurrNode = newCurrNode.next;
     }
-    console.log(newArray)
 
-    for(let i = 0; i < newArray.length; i++){
+    for (let i = 0; i < newArray.length; i++) {
       const wordObj = {
         memory_value: newArray[i].memory_value,
         correct_count: newArray[i].correct_count,
         incorrect_count: newArray[i].incorrect_count,
         next: newArray[i].next
-      }
-      console.log('wordObj', wordObj)
+      };
       await LanguageService.postUserWords(
-      req.app.get('db'), 
-      newArray[i].id, 
-      wordObj
-      )
+        req.app.get("db"),
+        newArray[i].id,
+        wordObj
+      );
     }
-
-
-
-    // const wordObj = {
-    //   memory_value: currNode.value.memory_value,
-    //   correct_count: currNode.value.correct_count,
-    //   incorrect_count: currNode.value.incorrect_count,
-    //   next: newNext
-    // }
-    
-    // await LanguageService.postUserWords(
-    //   req.app.get('db'), 
-    //   currNode.value.id, 
-    //   wordObj
-    //   )
 
     const langObj = {
       head: newArray[0].id,
-      total_score: language.total_score,
-    }
-    console.log('langObj', langObj)
+      total_score: language.total_score
+    };
+    console.log(newArray)
+    console.log("langObj", langObj);
 
-   
-  
     await LanguageService.postUserLanguage(
-      req.app.get('db'), 
-      currNode.value.language_id, 
+      req.app.get("db"),
+      currNode.value.language_id,
       langObj
-      )
-
+    );
   } catch (error) {
     next(error);
   }
 });
-
-// .then(currNode => {
-//   currNode = currNode.next;
-//   language.head = currNode.next;
-// });
-
-// languageRouter.put("/guess", bodyParser, async (req, res, next) => {
-//   const { wordCorrectCount, wordIncorrectCount } = req.body;
-//   const correct = wordCorrectCount;
-//   const incorrect = wordIncorrectCount;
-
-//   try {
-
-//   }
-//   catch(error) {
-//     throw new Error(error);
-//   }
-//   res.status(200);
-// })
 
 module.exports = languageRouter;
--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,4 +1,5 @@
 'use strict';
+const { LinkedList } = require('./LinkedList.js')
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
@@ -33,19 +34,52 @@ const LanguageService = {
   // update appropriate fields in word table once user responds with an answer
   // also need to update new 'next' pointer based on memory value!!!!
   postUserWords(db, id, wordObj) {
-    console.log(wordObj, 'id is: ' + id);
     return db('word')
       .update(wordObj)
       .where('id', id);
   },
   // update appropriate fields in language table once user responds with an answer, pointing to the new head
   postUserLanguage(db, id, langObj) {
-    console.log(langObj, 'langid is: ' + id);
     return db('language')
       .update(langObj)
       .where('id', id);
   },
-  
+  getNextWord(db, language_id){
+    return db
+      .from('word')
+      .join('language', 'word.id','=','language.head')
+      .select(
+        'original',
+        'language_id',
+        'correct_count',
+        'incorrect_count'
+      )
+      .where({language_id});
+  },
+  createLinkedList(words, head){
+    const headObj = words.find(word => word.id === head);
+    const headIndex = words.indexOf(headObj);
+    const headNode = words.splice(headIndex,1);
+    const list = new LinkedList();
+    list.insertLast(headNode[0]);
+
+    let nextId = headNode[0].next;
+    let currentWord = words.find(word => word.id === nextId);
+    list.insertLast(currentWord);
+    nextId = currentWord.next;
+    currentWord = words.find(word => word.id === nextId);
+
+    while(currentWord !== null){
+      list.insertLast(currentWord);
+      nextId = currentWord.next;
+      if(nextId === null){
+        currentWord = null;
+      } else {
+        currentWord = words.find(word => word.id === nextId);
+      }
+    }
+    return list;
+  },
 };
 
 module.exports = LanguageService;
