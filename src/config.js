20d05740968b76033fbdf092d9ff70a28e05f7f0
andrewbituin <fandrewbituin@gmail.com>
2019-04-19 08:54:26 -0700

    changed config file for deployment

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL
-    || 'postgresql://danielors@localhost/spaced-repetition',
+  // DB_URL: process.env.DB_URL
+  //   || 'postgresql://danielors@localhost/spaced-repetition',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://danielors@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
   CLIENT_ORIGIN: 'http://localhost:3000' // process.env.CLIENT_ORIGIN || 
